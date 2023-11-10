7a826d2a9567312002f2c84385efe5cbf9ec1d2d
Mxolisi Fikeni <mlfikeni@gmail.com>
2023-11-10 07:38:36 +0200

    Add project files.

---
+++ TaxCalculator.API/Controllers/TaxCalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TaxCalculator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxCalculatorController : ControllerBase
+    {       
+
+        // POST api/<TaxCalculatorController>
+        [HttpPost("CalculateTax")]
+        public async Task<IActionResult> Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
---
+++ TaxCalculator.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaxCalculator.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
