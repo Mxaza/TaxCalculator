f051d3952e6018a4867e84d28cf6447c7164266b
Mxolisi Fikeni <mlfikeni@gmail.com>
2023-11-10 10:09:53 +0200

    Added Initial Calculation Types

--- TaxCalculator.API/Program.cs
+++ TaxCalculator.API/Program.cs
@@ -1,3 +1,6 @@
+using TaxCalculator.Services.Implementations;
+using TaxCalculator.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ICalculationTypeService, CalculationTypeMockService>();
 
 var app = builder.Build();
 
---
+++ TaxCalculator.UnitTests/Controllers/CalculationTypesControllerTests.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TaxCalculator.API.Controllers;
+using TaxCalculator.Models;
+using TaxCalculator.Services.Interfaces;
+
+namespace TaxCalculator.UnitTests.Controllers
+{
+    public class CalculationTypesControllerTests
+    {      
+        [Test]
+        public async Task CalculationTypesController_CalculateTax_OnSuccess()
+        {
+            //Arrange
+            var mockLogger = new Mock<ILogger<CalculationTypesController>>();
+            var mockService = new Mock<ICalculationTypeService>();
+
+            var calculationTypes = new List<CalculationType>();
+
+            mockService.Setup(x => x.GetAll())
+                .Returns(Task.FromResult(calculationTypes));
+
+            //Act
+            var controller = new CalculationTypesController(mockLogger.Object, mockService.Object);
+
+            var result = await controller.GetAll();
+            var statusCodeResult = (ObjectResult?)result;
+            
+            //Assert
+            Assert.True(statusCodeResult.StatusCode == ResponseCodes.Ok);
+            
+        }
+
+        [Test]
+        public async Task CalculationTypesController_CalculateTax_OnNoContent()
+        {
+            //Arrange
+            var mockLogger = new Mock<ILogger<CalculationTypesController>>();
+            var mockService = new Mock<ICalculationTypeService>();
+
+            var calculationTypes = new List<CalculationType>();
+
+            mockService.Setup(x => x.GetAll())
+                .Returns(Task.FromResult(calculationTypes));
+
+            //Act
+            var controller = new CalculationTypesController(mockLogger.Object, mockService.Object);
+
+            var result = await controller.GetAll();
+            var statusCodeResult = (ObjectResult?)result;
+
+            //Assert            
+            Assert.True(statusCodeResult.StatusCode == ResponseCodes.NoContent);
+        }
+
+        public async Task CalculationTypesController_CalculateTax_OnInternalServerError()
+        {
+            //Arrange
+            var mockLogger = new Mock<ILogger<CalculationTypesController>>();
+            var mockService = new Mock<ICalculationTypeService>();
+
+            var calculationTypes = new List<CalculationType>();
+
+            mockService.Setup(x => x.GetAll())
+                .Returns(Task.FromResult(calculationTypes));
+
+            //Act
+            var controller = new CalculationTypesController(mockLogger.Object, mockService.Object);
+
+            var result = await controller.GetAll();
+            var statusCodeResult = (ObjectResult?)result;
+
+            //Assert
+            Assert.True(statusCodeResult.StatusCode == ResponseCodes.InternalServerError);
+        }        
+    }
+}
