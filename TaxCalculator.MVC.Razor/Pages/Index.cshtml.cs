ae732cbb1470f47984f8d8513fd141e29c671fab
Mxolisi Fikeni <mlfikeni@gmail.com>
2023-11-12 16:05:16 +0200

    Added mvc razor front-end

---
+++ TaxCalculator.MVC.Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TaxCalculator.Models;
+
+namespace TaxCalculator.MVC.Razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public double AnnualIncome { get; set; }
+
+        [BindProperty]
+        public string PostalCode { get; set; }
+
+        public string Result { get; set; }
+
+        public async Task OnPostAsync()
+        {
+            // Call the Web API endpoint
+            using (var httpClient = new HttpClient())
+            {
+                // TODO: Read from App Settings
+                var apiUrl = "https://localhost:7186/api/TaxCalculation/CalculateTax";
+
+                var requestData = new TaxCalculationRequest
+                {
+                    AnnualIncome = AnnualIncome,
+                    PostalCode = PostalCode
+                };
+
+                var response = await httpClient.PostAsJsonAsync(apiUrl, requestData);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Result = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    Result = "Error calculating tax. Please try again.";
+                }
+            }
+        }
+    }
+}
