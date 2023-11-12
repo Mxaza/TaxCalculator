5e1332e2e114522863a97d61dc231bf532c870a3
Mxolisi Fikeni <mlfikeni@gmail.com>
2023-11-12 17:31:56 +0200

    Added logic to dependency inject AppSettings into Index page.

--- TaxCalculator.MVC.Razor/Pages/Index.cshtml.cs
+++ TaxCalculator.MVC.Razor/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using TaxCalculator.Models;
 
 namespace TaxCalculator.MVC.Razor.Pages
@@ -7,27 +8,34 @@ namespace TaxCalculator.MVC.Razor.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Models.AppSettings _appSettings;        
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger, 
+            Models.AppSettings appSettings)
         {
             _logger = logger;
+            _appSettings = appSettings;
         }
 
         [BindProperty]
+        [Required(ErrorMessage = "Annual Income is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter a positive number for Annual Income.")]
         public double AnnualIncome { get; set; }
 
         [BindProperty]
+        [Required(ErrorMessage = "Postal Code is required.")]
+        [StringLength(4, ErrorMessage = "Postal Code must be 4 characters long.")]
         public string PostalCode { get; set; }
 
         public string Result { get; set; }
 
         public async Task OnPostAsync()
         {
-            // Call the Web API endpoint
+            // Call Web API endpoint
             using (var httpClient = new HttpClient())
             {
-                // TODO: Read from App Settings
-                var apiUrl = "https://localhost:7186/api/TaxCalculation/CalculateTax";
+                var apiUrl = _appSettings.ApplicationUrls.WebApiUrl;
 
                 var requestData = new TaxCalculationRequest
                 {
@@ -40,6 +48,10 @@ namespace TaxCalculator.MVC.Razor.Pages
                 if (response.IsSuccessStatusCode)
                 {
                     Result = await response.Content.ReadAsStringAsync();
+                }
+                if ((int)response.StatusCode == ResponseCodes.BadRequest)
+                {
+                    Result = "Error calculating tax. Please verify your input is correct and again.";
                 }
                 else
                 {
--- TaxCalculator.MVC.Razor/Program.cs
+++ TaxCalculator.MVC.Razor/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .Build();
+
+var appSettings = configuration.Get<TaxCalculator.MVC.Razor.Models.AppSettings>();
+builder.Services.AddSingleton(appSettings);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
